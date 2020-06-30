f0dc72a7b080b2f7444418e00aff537ce7afa977
ericksicard <esrfree@gmail.com>
2020-06-29 23:40:34 -0400

    Posting on MERN Social - Listing posts

---
+++ client/post/PostList.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import Post from './Post'
+
+/*PostList component will render any list of posts provided to it,
+which we can use in both the Newsfeed and the Profile components.*/
+
+export default function PostList(props) {
+    return (
+        <div style={{marginTop: '24px'}}>
+            {props.posts.map( (post, i) => {
+                return <Post post={post} key={i} onRemove={props.removeUpdate}/>
+            })}
+        </div>
+    )
+}
+
+PostList.propTypes = {
+    posts: PropTypes.array.isRequired,
+    removeUpdate: PropTypes.func.isRequired
+}
